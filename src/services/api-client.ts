3c10a1df153cd7b1916a57d3597477480a056333
Joshua Donato <jdonato@houstoneng.com>
2023-05-29 11:35:01 -0400

    Refactoring- fetch games with react-query

---
+++ src/services/api-client.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "638efa5741d94eccb4fa0e61cf912ea0",
+  },
+});
--- src/services/api-clients.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key:'638efa5741d94eccb4fa0e61cf912ea0'
-    }
-})
